b8b371d663e9e1291fecb7cd891b3b9a0c0fb815
Ignacio Chavez <nashio@gmail.com>
2015-08-15 21:51:10 -0700

    Update constants.js

--- app/js/utils/constants.js
+++ app/js/utils/constants.js
@@ -5,7 +5,7 @@ var constants = {
     posterSize : 'w1000',
     apiKey      : '<INSERT YOUR API KEY HERE>',
 
-    // using keyword 2157 (movies with the hacker keyword)
+    // 2157 corresponds to the keyword 'hacker'
     listAPI     : 'https://api.themoviedb.org/3/keyword/2157/movies?api_key=',
     singleAPI   : 'https://api.themoviedb.org/3/movie/'
 };
